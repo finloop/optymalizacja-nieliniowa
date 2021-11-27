5ba54298ea8be6253fd0bb4f2e6ef40be4de0b78
Piotr Krawiec <piotr.krawiec23@gmail.com>
2021-11-27 15:43:32 +0100

    Add bisect alg

--- shell.nix
+++ shell.nix
@@ -8,6 +8,7 @@ pkgs.mkShell {
     pkgs.rstudioWrapper
     pkgs.rPackages.magick
     pkgs.rPackages.tidyverse
+    pkgs.rPackages.numDeriv
     # keep this line if you use bash
     pkgs.bashInteractive
     pkgs.ffmpeg
